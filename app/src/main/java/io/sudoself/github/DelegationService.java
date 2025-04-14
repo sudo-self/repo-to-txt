09475cee63ccdbeab739776afa56a6285097573c
System Administrator <root@Jesse.local>
2025-04-14 05:49:42 -0600

    repo2txt

---
+++ app/src/main/java/io/sudoself/github/DelegationService.java
@@ -0,0 +1,14 @@
+package io.sudoself.github;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
